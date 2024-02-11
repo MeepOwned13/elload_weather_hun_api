747cecfb13e869533e13461ed8b7f2542d7b79a9
MeepOwned13 <sebokmatyas01@gmail.com>
2024-02-11 19:45:35 +0100

    Downloading omsz data
    'akt' works for sure, others not yet tested

--- src/downloader.py
+++ src/downloader.py
@@ -5,6 +5,7 @@ import sqlite3
 from contextlib import closing
 import pandas as pd
 import io
+from zipfile import ZipFile
 
 omsz_logger = logging.getLogger("omsz")
 omsz_logger.setLevel(logging.DEBUG)
@@ -65,4 +66,32 @@ class OMSZ_Downloader():
                                        parse_dates=["StartDate", "EndDate"], date_format="%Y%m%d")
         self._write_meta(self._format_meta(df))
         omsz_logger.info("Metadata updated to database")
+
+    def _format_weather(self, df: pd.DataFrame):
+        df.columns = df.columns.str.strip()  # remove trailing whitespace
+        df.index = df['Time']
+        df.drop('Time', axis=1, inplace=True)  # index creates duplicate
+        df.dropna(how='all', axis=1, inplace=True)  # remove NaN columns
+        return df
+
+    def _download_zipped_csv(self, url: str) -> pd.DataFrame | None:
+        """
+        Downloads given csv in zip at url
+        :param url: Url to ZIP
+        :return: Downloaded DataFrame
+        """
+        omsz_logger.info(f"Requesting zipped csv at '{url}'")
+        request = req_get(url)
+        if request.status_code != 200:
+            omsz_logger.error(f"Zipped csv download failed with {request.status_code} | {url}")
+            return
+        omsz_logger.debug(f"Zipped csv recieved from '{url}'")
+
+        with ZipFile(io.BytesIO(request.content), 'r') as zip_file:
+            df: pd.DataFrame = pd.read_csv(zip_file.open(zip_file.namelist()[0]), skiprows=6,  # skip metadata of csv
+                                           sep=';', skipinitialspace=True, na_values=['EOR', -999], low_memory=False,
+                                           parse_dates=['Time'], date_format="%Y%m%d%H%M"
+                                           )
+
+        return self._format_weather(df)
 
