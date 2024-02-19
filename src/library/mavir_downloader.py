405bdc2314a0067de3eb85a452de6ae4d54fe33d
MeepOwned13 <sebokmatyas01@gmail.com>
2024-02-19 17:08:28 +0100

    Fix MAVIR choose_update

--- src/library/mavir_downloader.py
+++ src/library/mavir_downloader.py
@@ -257,6 +257,6 @@ class MAVIR_Downloader():
         """
         end: pd.Timestamp = self._get_end_date_netload()
         now: pd.Timestamp = pd.Timestamp.now("UTC").tz_localize(None)
-        if now > (end + pd.Timedelta(hours=1)):
+        if now > end:
             self.update_electricity_data()
 
