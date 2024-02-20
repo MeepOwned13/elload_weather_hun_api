0a50a8398071da346bbd887c3e95550480355ee0
MeepOwned13 <sebokmatyas01@gmail.com>
2024-02-20 20:36:25 +0100

    2 new API calls
    Proper indexing in returned json, had to specify DataFrame index
    
    - get specified weather station for timeframe
    - get all weather stations for timeframe

--- src/library/reader.py
+++ src/library/reader.py
@@ -2,6 +2,8 @@ import logging
 from pathlib import Path
 import sqlite3
 import pandas as pd
+import numpy as np
+from datetime import datetime
 
 
 reader_logger = logging.getLogger("omsz")
@@ -34,15 +36,111 @@ class Reader():
             return res
         return execute
 
+    def _check_int(self, integer, name: str):
+        """
+        Check if arg is integer
+        :param integer: check this
+        :param name: name to write into error message
+        :return: None
+        :raises TypeError: if arg is not an int
+        """
+        if type(integer) is not int:
+            raise TypeError(f"Param {name} is not of type int")
+
+    def _check_date(self, date, name: str):
+        """
+        Check if arg is pandas.Timestamp or datetime.datetime
+        :param date: check this
+        :param name: name to write into error message
+        :return: None
+        :raises TypeError: if arg is not a pandas.Timestamp or datetime.datetime
+        """
+        if type(date) is not pd.Timestamp and type(date) is not datetime:
+            raise ValueError(f"Param {name} is not of type pandas.Timestamp or datetime.datetime")
+
+    def _limit_timeframe(self, start_date: pd.Timestamp | datetime, end_date: pd.Timestamp | datetime, minutes: int):
+        """
+        Check if end_date - start_date is over the limit
+        :param start_date: Start date in UTC
+        :param end_date: End date in UTC
+        :return: None
+        :raises ValueError: if the timeframe is over the limit
+        """
+        if end_date - start_date > pd.Timedelta(minutes=minutes):
+            raise ValueError(f"Given timeframe is over the {minutes} minute limit")
+
     @_db_transaction
     def get_weather_meta(self) -> pd.DataFrame:
         reader_logger.info("Reading OMSZ_meta")
         df = pd.read_sql("SELECT * FROM OMSZ_meta", con=self._con)
+        df.set_index("StationNumber", drop=True, inplace=True)
         return df
 
     @_db_transaction
     def get_electricity_meta(self) -> pd.DataFrame:
         reader_logger.info("Reading MAVIR_meta")
         df = pd.read_sql("SELECT * FROM MAVIR_meta", con=self._con)
+        df.set_index("Column", drop=True, inplace=True)
         return df
+
+    @_db_transaction
+    def get_weather_station(self, station: int, start_date: pd.Timestamp | datetime,
+                            end_date: pd.Timestamp | datetime) -> pd.DataFrame | None:
+        """
+        Get weather for given station in given timeframe
+        :param station: Which station to read from
+        :param start_date: Date to start at in UTC
+        :param end_date: Date to end on in UTC
+        :return: pandas.DataFrame with the data or None
+        """
+        self._check_int(station, "station")
+        self._check_date(start_date, "start_date")
+        self._check_date(end_date, "end_date")
+
+        self._limit_timeframe(start_date, end_date, (3 * 365 + 2 * 366) * 24 * 60)  # (at least) 5 years
+
+        exists = self._curs.execute(f"SELECT StationNumber FROM OMSZ_meta WHERE StationNumber = {station}").fetchone()
+        if not exists:
+            raise LookupError(f"Can't find station {station}")
+
+        reader_logger.info(f"Reading OMSZ_{station} from {start_date} to {end_date}")
+
+        df = pd.read_sql(f"SELECT * FROM OMSZ_{station} "
+                         f"WHERE Time BETWEEN datetime(\"{start_date}\") AND datetime(\"{end_date}\")",
+                         con=self._con)
+        df.set_index("Time", drop=True, inplace=True)
+
+        return df
+
+    @_db_transaction
+    def get_weather_time(self, start_date: pd.Timestamp | datetime, end_date: pd.Timestamp | datetime,
+                         df_to_dict: dict | None = None) -> dict:
+        """
+        Get weather for all stations in given timeframe
+        :param start_date: Date to start at in UTC
+        :param end_date: Date to end on in UTC
+        :return: dict{StationNumber: {Time: data}}
+        """
+        self._check_date(start_date, "start_date")
+        self._check_date(end_date, "end_date")
+
+        self._limit_timeframe(start_date, end_date, 7 * 24 * 60)  # 1 week
+
+        df_to_dict = df_to_dict or {"orient": "index"}
+
+        stations = pd.read_sql("SELECT StationNumber, StartDate, EndDate FROM OMSZ_meta",
+                               con=self._con, parse_dates=["StartDate", "EndDate"])
+
+        reader_logger.info(f"Reading all stations from {start_date} to {end_date}")
+
+        result = dict()
+        for _, row in stations.iterrows():
+            if row["StartDate"] is not pd.NaT and row["EndDate"] is not pd.NaT:
+                df = pd.read_sql(f"SELECT * FROM OMSZ_{row['StationNumber']} "
+                                 f"WHERE Time BETWEEN datetime(\"{start_date}\") AND datetime(\"{end_date}\")",
+                                 con=self._con)
+                df.set_index("Time", drop=True, inplace=True)
+                result[row["StationNumber"]] = df.replace({np.nan: None}).to_dict(**df_to_dict)
+
+        return result
 
--- src/main.py
+++ src/main.py
@@ -4,10 +4,13 @@ from pathlib import Path
 import library.omsz_downloader as o_dl
 import library.mavir_downloader as m_dl
 import library.reader as rd
+import pandas as pd
 import uvicorn
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi_utils.tasks import repeat_every
+from datetime import datetime
+import numpy as np
 
 logger = logging.getLogger("app")
 db_path = Path(f"{__file__}/../../data/sqlite.db").resolve()
@@ -15,8 +18,12 @@ omsz_dl = o_dl.OMSZ_Downloader(db_path)
 mavir_dl = m_dl.MAVIR_Downloader(db_path)
 reader = rd.Reader(db_path)
 
+DEV_MODE = False
 OMSZ_MESSAGE = "Weather data is from OMSZ, source: (https://odp.met.hu/)"
 MAVIR_MESSAGE = "Electricity data is from MAVIR, source: (https://mavir.hu/web/mavir/rendszerterheles)"
+DEFAULT_TO_DICT = {
+    "orient": "index",
+}
 
 
 @asynccontextmanager
@@ -31,6 +38,8 @@ app = FastAPI(lifespan=lifespan)
 
 @repeat_every(seconds=30)
 async def update_check():
+    if DEV_MODE:
+        return
     logger.info("Checking for updates to data sources")
     omsz_dl.choose_curr_update()
     mavir_dl.choose_update()
@@ -41,15 +50,34 @@ async def index():
     return {"Message": f"{OMSZ_MESSAGE}, {MAVIR_MESSAGE}"}
 
 
-@app.get("/omsz_meta/")
+@app.get("/omsz/meta/")
 async def get_omsz_meta():
-    df = reader.get_weather_meta()
-    return {"Message": OMSZ_MESSAGE, "data": df.to_json()}
+    df: pd.DataFrame = reader.get_weather_meta()
+    return {"Message": OMSZ_MESSAGE, "data": df.to_dict(**DEFAULT_TO_DICT)}
 
-@app.get("/mavir_meta/")
+
+@app.get("/omsz/station")
+async def get_weather_station(station: int, start_date: datetime, end_date: datetime):
+    try:
+        df: pd.DataFrame = reader.get_weather_station(station, start_date, end_date)
+    except (LookupError, ValueError) as error:
+        raise HTTPException(status_code=400, detail=str(error))
+    return {"Message": OMSZ_MESSAGE, "data": df.replace({np.nan: None}).to_dict(**DEFAULT_TO_DICT)}
+
+
+@app.get("/omsz/all")
+async def get_weather_time(start_date: datetime, end_date: datetime):
+    try:
+        result: dict = reader.get_weather_time(start_date, end_date, df_to_dict=DEFAULT_TO_DICT)
+    except (LookupError, ValueError) as error:
+        raise HTTPException(status_code=400, detail=str(error))
+    return {"Message": OMSZ_MESSAGE, "data": result}
+
+
+@app.get("/mavir/meta/")
 async def get_mavir_meta():
-    df = reader.get_electricity_meta()
-    return {"Message": MAVIR_MESSAGE, "data": df.to_json()}
+    df: pd.DataFrame = reader.get_electricity_meta()
+    return {"Message": MAVIR_MESSAGE, "data": df.to_dict(**DEFAULT_TO_DICT)}
 
 
 def main(logger: logging.Logger, skip_checks: bool):
@@ -57,10 +85,10 @@ def main(logger: logging.Logger, skip_checks: bool):
     logger.debug("Setting up")
     (db_path / "..").resolve().mkdir(exist_ok=True)
     # OMSZ init
-    if not skip_checks:
+    if not skip_checks and not DEV_MODE:
         omsz_dl.startup_sequence()
     # MAVIR init
-    if not skip_checks:
+    if not skip_checks and not DEV_MODE:
         mavir_dl.update_electricity_data()
 
     # Start the app
@@ -70,9 +98,11 @@ def main(logger: logging.Logger, skip_checks: bool):
 if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser(description="HUN Electricity and Weather API")
-    parser.add_argument("-sc", "--skip_checks", help="Skip startup DB download checks",
-                        action="store_true")
-    arg = parser.parse_args()
+    parser.add_argument("-sc", "--skip_checks", help="Skip startup DB download checks", action="store_true")
+    parser.add_argument("-d", "--dev", help="Developer mode, no downloads happen", action="store_true")
+    args = parser.parse_args()
+
+    DEV_MODE = args.dev
 
     # Set up logging
     log_folder = Path(f"{__file__}/../../logs").resolve()
@@ -102,5 +132,5 @@ if __name__ == "__main__":
     m_dl.mavir_downloader_logger.addHandler(log_ch)
     rd.reader_logger.addHandler(log_ch)
 
-    main(logger, arg.skip_checks)
+    main(logger, args.skip_checks)
 
